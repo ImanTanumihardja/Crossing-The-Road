cbce64fddb071df9b6d52c1526c1f1329d01886b
Iman Tanumihardja <34433210+ImanTanumihardja@users.noreply.github.com>
2018-06-08 21:23:47 -0700

    Revert "School Third Checkpoint"
    This reverts commit 9e016b7a8dd206885ae3af1d01d9d268f07eafb8.

---
+++ Assets/Scripts/CarMovement2.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement2 : MonoBehaviour
+{
+    public float speed = 10;
+    public float endPosition = 90;
+    private Vector3 resetCar;
+    // Use this for initialization
+    void Start()
+    {
+        resetCar = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
+        if (endPosition > 0)
+        {
+            if (gameObject.transform.position.x >= endPosition)
+            {
+                transform.position = resetCar;
+            }
+        }
+        else
+        {
+            if (gameObject.transform.position.x <= endPosition)
+            {
+                transform.position = resetCar;
+            }
+        }
+    }
+
+    public void SetSpeed(float modifier)
+    {
+        speed = speed + modifier;
+    }
+
+   
+}
